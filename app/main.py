a6bc80935bcbce6a0378e38db4d44e70d38a319a
youngroma <krivichroma228@gmail.com>
2025-02-10 14:09:57 +0100

    init database

--- app/main.py
+++ app/main.py
@@ -1,8 +1,10 @@
 from fastapi import FastAPI
 from app.config import settings
 from app.routes import router
+from database import SessionLocal, engine, Base
 import uvicorn
 
+Base.metadata.create_all(bind=engine)
 app = FastAPI()
 
 app.include_router(router)
