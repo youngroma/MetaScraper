9df87a98905936570e8b7a4f0e6ff3d7557ca9fc
youngroma <krivichroma228@gmail.com>
2025-02-11 23:10:59 +0100

    Connected routes from auth_router to the FastAPI main application, added decorator that is used to boot the configuration

--- app/main.py
+++ app/main.py
@@ -1,13 +1,21 @@
 from fastapi import FastAPI
+from fastapi_jwt_auth import AuthJWT
 from app.config import settings
+from app.auth import auth_router
 from app.routes import router
-from database import SessionLocal, engine, Base
+from app.database import SessionLocal, engine, Base
 import uvicorn
 
 Base.metadata.create_all(bind=engine)
 app = FastAPI()
 
+app.include_router(auth_router)
 app.include_router(router)
+
+@AuthJWT.load_config
+def get_config():
+    return settings
+
 
 if __name__ == "__main__":
     uvicorn.run(app, host='0.0.0.0', port=8080, reload=True, workers=3)
