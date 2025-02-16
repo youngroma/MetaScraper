1abc212c6f153b50baf09440118817961c2de133
youngroma <krivichroma228@gmail.com>
2025-02-16 15:01:44 +0100

    moved get_db func

--- app/database.py
+++ app/database.py
@@ -9,4 +9,11 @@ SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 Base = declarative_base()
 
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+
 
