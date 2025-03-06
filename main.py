c8a916eb025edeac64af30b7f100b26e047d7ac0
youngroma <krivichroma228@gmail.com>
2025-03-06 15:29:49 +0100

    Added Prometheus for monitoring application

--- main.py
+++ main.py
@@ -1,13 +1,18 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
 from fastapi_jwt_auth import AuthJWT
 from app.config import settings
 from app.auth import auth_router
 from app.routes import router
 from app.database import engine, Base
 import uvicorn
+from prometheus_fastapi_instrumentator import Instrumentator
+import logging
+
 
 Base.metadata.create_all(bind=engine)
 app = FastAPI()
+instrumentator = Instrumentator()
+instrumentator.instrument(app).expose(app)
 
 app.include_router(auth_router)
 app.include_router(router)
@@ -16,6 +21,7 @@ app.include_router(router)
 def get_config():
     return settings
 
+logging.basicConfig(level=logging.INFO, format="%(asctime)s - %(levelname)s - %(message)s")
 
 if __name__ == "__main__":
     uvicorn.run(app, host='0.0.0.0', port=8080, reload=True, workers=3)
